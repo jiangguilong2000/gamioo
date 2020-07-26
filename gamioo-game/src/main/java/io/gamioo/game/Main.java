79675f51d4e031e5e5fe08b21f28e373d6d1701a
Allen Jiang <41157121@qq.com>
2020-07-27 02:34:00 +0800

    测试代码 测试代码测试代码测试代码

--- gamioo-game/src/main/java/io/gamioo/game/Main.java
+++ gamioo-game/src/main/java/io/gamioo/game/Main.java
@@ -3,7 +3,7 @@ package io.gamioo.game;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello,soyabean,hahaha");
+		System.out.println("hello,soyabean");
 	}
 
 }
