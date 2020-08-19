56d9aacd4cb5283799363a392a89ffe1812db97c
Allen Jiang <41157121@qq.com>
2020-08-19 19:59:53 +0800

    可命名别名的线程工厂类

---
+++ gamioo-core/src/main/java/io/gamioo/core/concurrent/GameThreadFactory.java
@@ -0,0 +1,30 @@
+package io.gamioo.core.concurrent;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 线程工厂,可以设置别名
+ *
+ * @author Allen Jiang[41157121@qq.com]
+ * @since 1.0
+ */
+public class GameThreadFactory implements ThreadFactory {
+	private final String name;
+	private final AtomicInteger threadCounter = new AtomicInteger(0);
+
+	public Thread newThread(Runnable runnable) {
+		StringBuilder threadName = new StringBuilder(name);
+		threadName.append("-").append(threadCounter.getAndIncrement());
+		Thread thread = new Thread(group, runnable, threadName.toString());
+		return thread;
+	}
+
+	final ThreadGroup group;
+
+	public GameThreadFactory(String name) {
+		SecurityManager securitymanager = System.getSecurityManager();
+		this.group = securitymanager == null ? Thread.currentThread().getThreadGroup() : securitymanager.getThreadGroup();
+		this.name = name;
+	}
+}
