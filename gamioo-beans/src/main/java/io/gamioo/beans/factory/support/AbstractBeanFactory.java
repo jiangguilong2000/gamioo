675d94a28960d8132f6b636a02251dfb867925b1
Allen Jiang <41157121@qq.com>
2020-08-29 18:51:05 +0800

    加上版权信息和文件头

--- gamioo-beans/src/main/java/io/gamioo/beans/factory/support/AbstractBeanFactory.java
+++ gamioo-beans/src/main/java/io/gamioo/beans/factory/support/AbstractBeanFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015-2020 Gamioo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.gamioo.beans.factory.support;
 
 import io.gamioo.beans.config.BeanDefinition;
@@ -7,6 +23,12 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * some description
+ *
+ * @author Allen Jiang
+ * @since 1.0.0
+ */
 public abstract class AbstractBeanFactory implements BeanFactory {
 
     private final Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>();
