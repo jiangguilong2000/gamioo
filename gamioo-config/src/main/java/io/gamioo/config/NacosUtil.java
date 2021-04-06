7b51dff221d32859a733cfe3f029868b0b1425af
Allen Jiang <41157121@qq.com>
2021-04-06 16:15:22 +0800

    配置中心

--- gamioo-config/src/main/java/io/gamioo/config/NacosUtil.java
+++ gamioo-config/src/main/java/io/gamioo/config/NacosUtil.java
@@ -4,8 +4,10 @@ package io.gamioo.config;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.nacos.api.NacosFactory;
 import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.ConfigType;
 import com.alibaba.nacos.api.config.listener.Listener;
 import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.shaded.com.google.protobuf.ServiceException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,35 +18,37 @@ import java.util.concurrent.Executor;
 public class NacosUtil {
     private static final Logger logger = LogManager.getLogger(NacosUtil.class);
 
-    @PostConstruct
-    public void initNacos() {
+    public static void main(String[] args) throws ServiceException {
+        NacosUtil util=new NacosUtil();
+        util.initNacos();
+    }
+    public void initNacos() throws ServiceException {
         logger.info("[nacos]:nacosbutton equals 1L");
         ServerConfigManager serverConfigManager = new ServerConfigManager();
-        serverConfigManager.load("game","config/game-config.yml");
+        serverConfigManager.load("game","game-config.yml");
         ServerConfig config=serverConfigManager.getServerConfig();
         //指定连接到的服务器地址，可以是本地也可以是其他服务器
-        String serverAddr = "1.15.9.113:8848";
+        String serverAddr = "1.15.9.113";
         //自己指定的组
-        String group = "test-group";
+        String group = "game";
 
         // 示例
         try {
             //自己指定的dataId
-            String dataId = "test-dataId";
+            String dataId = "10003";
 
             //从nacos中获取的开关服务
-            ConfigService configService = getConfigService(serverAddr);
+            ConfigService configService = this.getConfigService(serverAddr);
 
             //本方法启动的时候获取内容
             String content = configService.getConfig(dataId, group, 5000);
             logger.info("content:{}", content);
-            JSONObject object = JSONObject.parseObject(content);
-
             //获取服务器状态
             String status = configService.getServerStatus();
             logger.debug("status={}", status);
 
-            configService.publishConfig(dataId, group, config.getContent());
+            configService.publishConfig(dataId, group, config.getContent(), ConfigType.YAML.getType());
+
 
 
             //监听器，一旦nacos中相应值改变，则进行相应开关状态改变
@@ -53,7 +57,7 @@ public class NacosUtil {
                 public void receiveConfigInfo(String configInfo) {
                     logger.info("[Listener]:{}", configInfo);
 
-                    JSONObject object = JSONObject.parseObject(configInfo);
+
                 }
 
                 @Override
@@ -62,7 +66,7 @@ public class NacosUtil {
                 }
             });
         } catch (NacosException e) {
-            logger.error("获取功能开关配置失败:" + e, e.getMessage());
+            logger.error("获取功能开关配置失败:" + e, e);
         }
 
 
