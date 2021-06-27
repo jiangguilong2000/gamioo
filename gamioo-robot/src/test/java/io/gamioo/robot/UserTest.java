36f41338b46d621e9e1bcdaf151c5869013c67d6
Allen Jiang <41157121@qq.com>
2021-06-27 20:31:58 +0800

    robot upgrade

--- gamioo-robot/src/test/java/io/gamioo/robot/UserTest.java
+++ gamioo-robot/src/test/java/io/gamioo/robot/UserTest.java
@@ -1,7 +1,10 @@
 package io.gamioo.robot;
 
+import com.alibaba.fastjson.JSON;
 import io.gamioo.core.util.FileUtils;
 import io.gamioo.core.util.StringUtils;
+import io.gamioo.redis.Redis;
+import io.gamioo.redis.RedisConstant;
 import io.gamioo.robot.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,32 +25,30 @@ public class UserTest {
     private static final Logger logger = LogManager.getLogger(UserTest.class);
 
     public static void main(String[] args) {
+        Redis redis = new Redis("106.53.236.198", 6385, "123.123");
+
         UserTest test = new UserTest();
-        Map<String, User> store = test.getUserList("user.txt");
-        Map<String, String> playerStore = test.getPlayerList("player.txt");
-        Map<String, String> remain = new HashMap<>();
-        Map<String, String> token = new HashMap<>();
-        for (User e : store.values()) {
-            token.put(e.getToken(), e.getToken());
-        }
-        for (String e : playerStore.keySet()) {
-            if (store.containsKey(e)) {
-                User user = store.get(e);
-
-                System.out.println(user.getId() + "," + user.getToken());
-            } else {
-                remain.put(e, e);
-
+        Map<Integer, User> store = test.getUserList("user.txt");
+        Map<Integer, User> storeB = test.getUserList("user2.txt");
+        int i = 1;
+        int size = 0;
+        for (User entry : store.values()) {
+            User user = storeB.get(entry.getId());
+            if (user != null) {
+                entry.setGroup(i);
+                entry.setTokenB(user.getToken());
+                size++;
+                if (size % 20 == 0) {
+                    size = 0;
+                    i++;
+                }
             }
         }
-        for (String e : remain.keySet()) {
-            System.out.println(e);
+        for (User user : store.values()) {
+            redis.hset(RedisConstant.KEY_ROBOT, String.valueOf(user.getId()), JSON.toJSONString(user));
         }
+        System.out.println(redis.hlen(RedisConstant.KEY_ROBOT));
 
-        System.out.println("................................");
-        for (String e : token.keySet()) {
-            System.out.println(e);
-        }
     }
 
     public Map<String, String> getPlayerList(String path) {
@@ -66,8 +67,8 @@ public class UserTest {
         return ret;
     }
 
-    public Map<String, User> getUserList(String path) {
-        Map<String, User> ret = new HashMap<>();
+    public Map<Integer, User> getUserList(String path) {
+        Map<Integer, User> ret = new HashMap<>();
 
         File file = FileUtils.getFile(path);
         try {
@@ -75,9 +76,9 @@ public class UserTest {
             content.forEach((value) -> {
                 String[] array = StringUtils.split(value, ",");
                 User user = new User();
-                user.setId(Long.parseLong(array[0]));
+                user.setId(Integer.parseInt(array[0]));
                 user.setToken(array[1]);
-                ret.put(String.valueOf(user.getId()), user);
+                ret.put(user.getId(), user);
             });
 
         } catch (IOException e) {
