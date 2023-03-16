3cfdcbea86784dbed3f61aa6448cbccb5f906ee7
Allen Jiang <41157121@qq.com>
2023-03-16 15:19:07 +0800

    chore(package-info): 新增默认文件

--- gamioo-event/src/main/java/io/gamioo/event/Main.java
+++
@@ -1,9 +0,0 @@
-package io.gamioo.event;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("1111");
-	}
-
-}
--- gamioo-orm/src/main/java/io/gamioo/orm/Main.java
+++
@@ -1,9 +0,0 @@
-package io.gamioo.orm;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("1111");
-	}
-
-}
